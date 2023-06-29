0c037117d722dd8d0bfe342d08d88aae8d591e94
Hud Wahab <hudwahab@gmail.com>
2023-06-29 13:21:50 -0600

    add v1 - better

---
+++ after_v1.py
@@ -0,0 +1,40 @@
+import unittest
+
+
+def count_fruits(fruits: list[str]) -> dict[str, int]:
+    fruit_counts: dict[str, int] = {}
+    for fruit in fruits:
+        if fruit in fruit_counts:
+            fruit_counts[fruit] += 1
+        else:
+            fruit_counts[fruit] = 1
+    return fruit_counts
+
+
+class TestCountFruits(unittest.TestCase):
+    def test_count_fruits_default(self):
+        self.assertEqual(
+            count_fruits(
+                [
+                    "apple",
+                    "banana",
+                    "apple",
+                    "cherry",
+                    "banana",
+                    "cherry",
+                    "apple",
+                    "apple",
+                    "cherry",
+                    "banana",
+                    "cherry",
+                ]
+            ),
+            {"apple": 4, "banana": 3, "cherry": 4},
+        )
+
+    def test_count_fruits_empty(self):
+        self.assertEqual(count_fruits([]), {})
+
+
+if __name__ == "__main__":
+    unittest.main()
