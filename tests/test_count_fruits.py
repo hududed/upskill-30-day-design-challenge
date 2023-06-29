9172decd401ea88db4736a931814f6f6eeb93b75
Hud Wahab <hudwahab@gmail.com>
2023-06-29 12:45:44 -0600

    adding tests

---
+++ tests/test_count_fruits.py
@@ -0,0 +1,22 @@
+from after import count_fruits
+
+
+def test_count_fruits_default() -> None:
+    assert count_fruits(
+        [
+            "apple",
+            "banana",
+            "apple",
+            "cherry",
+            "banana",
+            "cherry",
+            "apple",
+            "apple",
+            "cherry",
+            "banana",
+            "cherry",
+        ]
+    ) == {"apple": 4, "banana": 3, "cherry": 4}
+
+def test_count_fruits_empty() -> None:
+    assert count_fruits([]) == {}
